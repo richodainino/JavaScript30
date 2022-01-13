5a2c61270c77f663ae991a07c01f4093acc697c2
Richo <daininoricho@yahoo.com>
2022-01-14 00:20:43 +0800

    Fixing typos and clearing up the regex

--- 17 - Sort Without Articles/script.js
+++ 17 - Sort Without Articles/script.js
@@ -1,7 +1,7 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
 function strip(bandName) {
-  return bandName.replace(/^(an?|the)\s+?/i, "").trim();
+  return bandName.replace(/^(an?|the)\s/i, "").trim();
 }
 
 const sortedBands = bands.sort((firstBand, secondBand) => 
