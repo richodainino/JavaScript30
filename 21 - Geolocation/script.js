4bbfbf6bc28cd15b4827864d674392973afa2f6c
Richo <daininoricho@yahoo.com>
2022-01-17 23:58:31 +0800

    Rounding up the speed to 2 decimal places

--- 21 - Geolocation/script.js
+++ 21 - Geolocation/script.js
@@ -2,9 +2,13 @@ const arrow = document.querySelector('.arrow');
 const speed = document.querySelector('.speed-value');
 
 navigator.geolocation.watchPosition((data) => {
-  speed.textContent = data.coords.speed;
+  if (!(data.coords.speed)){
+   alert("I'm sorry this browser can't be used to show heading and speed");
+  }
+  const speeds = (data.coords.speed * 3.6);
+  speed.textContent = Math.round((speeds + Number.EPSILON) * 100) / 100;
   arrow.style.transform = `rotate(${data.coords.heading}deg)`;
 }, (err) => {
   console.err(err);
-  alert('Please allow the location access!');
+  alert('Please allow the location access');
 });
