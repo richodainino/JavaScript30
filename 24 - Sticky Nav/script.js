6dd2a3d65143b7de3fffd7a5711f828a737487ee
Richo <daininoricho@yahoo.com>
2022-01-20 19:43:18 +0800

    Add debounce function

--- 24 - Sticky Nav/script.js
+++ 24 - Sticky Nav/script.js
@@ -1,6 +1,21 @@
 const nav = document.querySelector('nav');
 const topOfNav = nav.offsetTop;
 
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
 function fixNav() {
   if (window.scrollY >= topOfNav) {
     document.body.style.paddingTop = nav.offsetHeight + 'px';
@@ -12,4 +27,4 @@ function fixNav() {
   }
 }
 
-window.addEventListener('scroll', fixNav);
+window.addEventListener('scroll', debounce(fixNav));
